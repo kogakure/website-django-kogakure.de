c5fc3eaead25fdcb8c5b6e2908b1aa77b532411e
Stefan Imhoff <stefan@imhoff.name>
2009-10-17 18:25:47 +0200

    Update on syntax for Fabric 0.9+

--- kogakure/fabfile.py
+++ kogakure/fabfile.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# This is a deployment script for "Fabric", a python-based tool like
-# Capistrano. You can get it at http://www.nongnu.org/fab/
-# Change the values in with config.xyz and start the command with "fab", e.g.
+# This is a deployment script for "Fabric" (0.9+), a python-based tool like
+# Capistrano. You can get it at http://www.fabfile.org/
+# Change the values in env.### and start the command with "fab", e.g.
 # fab restart_server
 
-config.fab_hosts = ['hostname.com']
-config.server_path = '/path/to/bin'
-config.project_path = '/path/to/django/project/root'
-config.memcached_ip = '127.0.0.1' # IP of memcached
-config.memcached_port = '1234' # Port of memcached
-config.memcached_size = '20' # in MByte
+from fabric.api import env, local, run
+
+env.hosts = ['hostname.com']
+env.server_path = '/path/to/bin'
+env.project_path = '/path/to/django/project/root'
+env.memcached_ip = '127.0.0.1' # IP of memcached
+env.memcached_port = '1234' # Port of memcached
+env.memcached_size = '20' # in MByte
 
 def deploy():
     "Push local changes to server, pull changes on server, restart server"
-    local('git push;', fail='warn')
-    run('cd $(project_path)/; git pull; delpyc', fail='warn')
+    local('git push;')
+    run('cd $(project_path)/; git pull; delpyc' % env, pty=True)
     restart_server()
     
 def stop_server():
     "Stop Apache"
-    run('$(server_path)/stop', fail='warn')
+    run('$(server_path)/stop' % env, pty=True)
 
 def start_server():
     "Start apache"
-    run('$(server_path)/start', fail='warn')
+    run('$(server_path)/start' % env, pty=True)
 
 def restart_server():
     "Restart Apache"
-    run('$(server_path)/stop', fail='warn')
-    run('$(server_path)/start', fail='warn')
+    run('$(server_path)/stop' % env, pty=True)
+    run('$(server_path)/start' % env, pty=True)
 
 def restart_memcached():
     "Restart Memcached, restart server"
-    run('kill `pgrep -u $LOGNAME memcached`', fail='warn')
-    run('/usr/local/bin/memcached -d -l $(memcached_ip) -m $(memcached_size) -p $(memcached_port)', fail='warn')
+    run('kill `pgrep -u $LOGNAME memcached`' % env, pty=True)
+    run('/usr/local/bin/memcached -d -l $(memcached_ip) -m $(memcached_size) -p $(memcached_port)' % env, pty=True)
     restart_server()
